1515d4684cc56b1333e564b23bd4e2018e633650
DESKTOP-RPI3CBJ\JK <worb0303@gmail.com>
2022-06-26 18:09:18 +0900

    - prettier 오류 처리 - useCallback으로 함수 재사용할 수 있도록 처리

--- src/pages/components/counter/Counter.js
+++ src/pages/components/counter/Counter.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./Counter.css";
 
 export default function Counter(props) {
   const [count, setCount] = useState(0);
   const { numberLabel, textLabel, number } = props;
   const duration = 2000;
-  const frameDuration = 1000 / 60;
 
   const getProgress = (t) => {
     return 1 - Math.pow(1 - t, 6.5);
   };
 
-  const timerFunction = () => {
+  const timerFunction = useCallback(() => {
     let start = 0;
+    const frameDuration = 1000 / 60;
     const totalFrames = Math.round(duration / frameDuration);
     const timer = setInterval(() => {
       start += 1;
@@ -22,11 +22,11 @@ export default function Counter(props) {
         clearInterval(timer);
       }
     }, frameDuration);
-  };
+  }, [number]);
 
   useEffect(() => {
     timerFunction();
-  }, []);
+  }, [timerFunction]);
 
   return (
     <div className="count">
